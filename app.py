673c8a536d0f1a45b3979c5f69a64ab23b247b01
Aman Malhi <065010@fsm.ac.in>
2025-12-19 02:01:58 +0530

    Add initial Streamlit app build-up

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Anime Recommendation System",
+    layout="wide"
+)
+
+st.title("🎌 Anime Recommendation System")
+st.write("App is loading correctly.")
 
